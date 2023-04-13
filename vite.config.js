be25227b152bbf56913ac36970088b85cf580a8b
stanosuozah <stanleyosuozah@gmail.com>
2023-04-13 23:28:49 +0100

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			external: [
+				"react", // ignore react stuff
+				"react-dom",
+			],
+		},
+	},
+});
